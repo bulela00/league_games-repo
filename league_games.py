322cf9632dca92549fc3aa38abe8dd023e5d1955
bulela00 <bulelandika@gmail.com>
2024-09-06 23:38:11 +0200

    adding project framework

---
+++ league_games.py
@@ -0,0 +1,46 @@
+# League opponents draw 
+
+#=======importing libraries=======
+
+
+#=======Functions=======
+# This function recieves the team information for the teams in the leagues 
+def league_teams(): 
+    # accepts teams' name
+    # teams' points 
+    # teams' location 
+    return 
+
+# This function splits the league teams into four groups based on the team points
+def league_groups(teams_list): 
+    # sort teams based on points 
+    # split the sorted list into four groups 
+    return 
+
+
+
+
+
+# Define teams in the league 
+# Accept input of team data for teams that have qualified for the team 
+# Team data includes: team location, team grouping 
+
+
+# Define constraints 
+
+
+
+# Define function that selects teams eligible to be opponents to selected team
+
+
+
+# Define order of the draw 
+
+
+# Accept random number for team selection 
+
+# Find out which oponents each team already has 
+
+
+
+# Create case for team selection of each group 
